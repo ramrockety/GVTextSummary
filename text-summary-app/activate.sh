1dd190f28e5cad46cfc8a7a04e06106240637191
Theideabased <ogunmusireseyi01@gmail.com>
2024-11-04 13:29:31 +0100

    addding a sample api for the different service

---
+++ text-summary-app/activate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+chmod +x fnm_setup.sh
---
+++ text-summary-app/setup_fnm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Capture the output of 'fnm env --use-on-cd' and evaluate it in the current shell environment
+eval "$(fnm env --use-on-cd)"
